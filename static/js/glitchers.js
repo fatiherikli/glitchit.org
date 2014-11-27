689ed72b056eaa85cc0141bb746a752cf9efec9a
fatiherikli <fatih@hipo.biz>
2014-11-27 09:45:58 +0200

    Fix firefox glitching problem

--- static/js/glitchers.js
+++ static/js/glitchers.js
@@ -1,16 +1,45 @@
 glitchit.Base64ImageGlitcher = $.Class.extend({
-    glitch: function (imageData) {
+    to_base64: function (buffer) {
+        var binary = '';
+        var bytes = new Uint8Array(buffer);
+        var len = bytes.byteLength;
+        for (var i = 0; i < len; i++) {
+            binary += String.fromCharCode(bytes[ i ]);
+        }
+        return window.btoa(binary);
+    },
+
+    to_buffer: function (base64_value) {
+        var binary_string = window.atob(base64_value);
+        var len = binary_string.length;
+        var bytes = new Uint8Array(len);
+        for (var i = 0; i < len; i++)        {
+            var ascii = binary_string.charCodeAt(i);
+            bytes[i] = ascii;
+        }
+        return bytes;
+    },
+
+    glitch: function (imageData, iteration) {
+        iteration = iteration || 0;
         var indicator = 'base64,',
             parts = imageData.split(indicator),
-            data = atob(parts[1]),
-            prefix = parts[0] + indicator,
-            glitchLevel = 33333333;
+            data = this.to_buffer(parts[1]),
+            prefix = parts[0] + indicator;
+            
         for (var i=0; i < data.length; i++) {
-            var randomNumber = parseInt(Math.random() * (data.length / glitchLevel));
-            if (i % randomNumber == 0) {
-                data = data.replaceAt(i, data.charAt(i+1));
+            if (i>(data.length/20) && i < (data.length/3)) {
+                data[i] = data[i+50]
             }
+            
         }
-        return prefix + btoa(data);
+
+        var glitched = prefix + this.to_base64(data);
+
+        if (iteration < (Math.random() * 10)) {
+            return this.glitch(glitched, iteration + 1)
+        }
+        
+        return glitched;
     }
 });
